7769be3bb6685fd74365a576a364f65069d40f8f
Reignable <joebarrett401@gmail.com>
2017-02-13 14:01:34 +0000

    Removed redundant script

--- create_test_image.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-base_image = cv2.imread('v4/test_image.jpg')
-gray_scaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray_scaled, (5, 5), 0)
-edged = cv2.Canny(blurred, 0, 100, apertureSize=3)
